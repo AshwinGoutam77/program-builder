6b2856c2967a27d441527cdb64ff715ddf2c8ed4
Ashwingoutam77 <ashwini@popipro.com>
2025-05-23 18:40:47 +0530

    mobile menu

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -130,12 +130,7 @@ export default function Header() {
                     <DialogPanel className="sidebar fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
                             <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                    className="h-8 w-auto"
-                                />
+                                <img src="/images/header-logo.png" alt="logo" />
                             </a>
                             <button
                                 type="button"
@@ -154,27 +149,25 @@ export default function Header() {
                                         href="#"
                                         className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                     >
-                                        Features
+                                        About us
                                     </a>
                                     <a
                                         href="#"
                                         className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                     >
-                                        Marketplace
+                                        Program Overview
                                     </a>
                                     <a
                                         href="#"
                                         className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                     >
-                                        Company
+                                        Our Alumni
                                     </a>
-                                </div>
-                                <div className="py-6">
                                     <a
                                         href="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                     >
-                                        Log in
+                                        Sarvan Teaches
                                     </a>
                                 </div>
                             </div>
